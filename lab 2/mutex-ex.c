1f37a83d03711215a4a823935f833bee2a7a8f14
Farewe1ll <Farewe1ll_youngjh@foxmail.com>
2025-05-13 18:32:44 +0800

    initaled mutex-ex.c and fixed pid_name

--- lab 2/mutex-ex.c
+++ lab 2/mutex-ex.c
@@ -2,17 +2,21 @@
 #include <pthread.h>
 
 int sum = 0;
+pthread_mutex_t mutex;
 
 void *thread() {
 	int i;
 	for (i = 0; i < 1000000; i++) {
+		pthread_mutex_lock(&mutex);
 		++sum;
+		pthread_mutex_unlock(&mutex);
 	}
 	return NULL;
 }
-
 int main() {
 	pthread_t tid1, tid2;
+	pthread_mutex_init(&mutex, NULL);
+
 	pthread_create(&tid1, NULL, thread, NULL);
 	pthread_create(&tid2, NULL, thread, NULL);
 	pthread_join(tid1, NULL);
