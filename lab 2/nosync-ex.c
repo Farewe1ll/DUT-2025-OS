2c2281eca1ed1e61f3f44ea03bc38c917603a249
Farewe1ll <Farewe1ll_youngjh@foxmail.com>
2025-05-13 18:23:27 +0800

    initialed nosync-ex.c

---
+++ lab 2/nosync-ex.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int sum = 0;
+
+void *thread() {
+	int i;
+	for (i = 0; i < 1000000; i++) {
+		++sum;
+	}
+}
+
+int main() {
+	pthread_t t1, t2;
+	pthread_create(&t1, NULL, thread, NULL);
+	pthread_create(&t2, NULL, thread, NULL);
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+	printf("1000000 + 1000000 = %d\n", sum);
+	return 0;
+}
